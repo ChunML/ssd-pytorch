060d4305d88e8bd8e429a2e328b95f1597f28ba9
Trung Tran <trungtran@machinetalk.org>
2019-06-03 05:44:07 +0000

    Day 1: use dataloader instead of accessing dataset directly

--- data.py
+++ data.py
@@ -8,6 +8,14 @@ from image_utils import ImageVisualizer
 
 
 class VOCDataset(Dataset):
+    """ Class for VOC Dataset
+
+    Attributes:
+        root_dir: dataset root dir (ex: ./data/VOCdevkit)
+        year: dataset's year (2007 or 2012)
+        num_examples: number of examples to be used
+                      (in case one wants to overfit small data)
+    """
     def __init__(self, root_dir, year, num_examples=-1):
         super(VOCDataset, self).__init__()
         self.idx_to_name = [
@@ -31,6 +39,17 @@ class VOCDataset(Dataset):
         return len(self.ids)
 
     def _get_image(self, index):
+        """ Method to read image from file
+            then resize to (300, 300)
+            then subtract by ImageNet's mean
+            then convert to Torch Tensor
+
+        Args:
+            index: the index to get filename from self.ids
+
+        Returns:
+            img: Torch tensor of shape (3, 300, 300)
+        """
         filename = self.ids[index]
         img_path = os.path.join(self.image_dir, filename + '.jpg')
         img = cv2.imread(img_path)
@@ -41,6 +60,18 @@ class VOCDataset(Dataset):
         return torch.from_numpy(img).permute(2, 0, 1), orig_shape
 
     def _get_annotation(self, index, orig_shape):
+        """ Method to read annotation from file
+            Boxes are normalized to image size
+            Integer labels are increased by 1
+
+        Args:
+            index: the index to get filename from self.ids
+            orig_shape: image's original shape
+
+        Returns:
+            boxes: numpy array of shape (num_gt, 4)
+            labels: numpy array of shape (num_gt,)
+        """
         h, w, _ = orig_shape
         filename = self.ids[index]
         anno_path = os.path.join(self.anno_dir, filename + '.xml')
@@ -62,6 +93,17 @@ class VOCDataset(Dataset):
         return np.array(boxes, dtype=np.float32), np.array(labels, dtype=np.int64)
 
     def __getitem__(self, index):
+        """ The __getitem__ method
+            so that the object can be iterable
+
+        Args:
+            index: the index to get filename from self.ids
+
+        Returns:
+            img: Torch tensor of shape (3, 300, 300)
+            boxes: Torch tensor of shape (num_gt, 4)
+            labels: Torch tensor of shape (num_gt,)
+        """
         img, orig_shape = self._get_image(index)
         boxes, labels = self._get_annotation(index, orig_shape)
         boxes = torch.from_numpy(boxes)
@@ -71,24 +113,50 @@ class VOCDataset(Dataset):
 
 
 def create_dataloader(root_dir, batch_size, num_examples=-1):
+    """ Create a DataLoader object
+        to iterate throughout the dataset
+
+    Args:
+        root_dir: root dir to the dataset
+        batch_size: batch size
+        num_examples: number of examples to use
+
+    Returns:
+        dataloader: an instance of DataLoader
+    """
     dataset = VOCDataset('./data/VOCdevkit', '2007', num_examples)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    return dataloader
+    info = {
+        'idx_to_name': dataset.idx_to_name,
+        'name_to_idx': dataset.name_to_idx,
+        'num_classes': len(dataset.idx_to_name)
+    }
+
+    return dataloader, info
 
 
 if __name__ == '__main__':
-    voc = VOCDataset('./data/VOCdevkit', '2007')
-    data = voc[0]
+    dataloader, info = create_dataloader('./data/VOCdevkit', 1)
+    data = next(iter(dataloader))
 
-    idx_to_name = voc.idx_to_name
+    idx_to_name = info['idx_to_name']
 
+    # Confirm data shapes
     print([x.shape for x in data])
 
     img, boxes, labels = data
+    # Eliminate the batch dimension
+    img = img.squeeze(0)
+    boxes = boxes.squeeze(0)
+    labels = labels.squeeze(0)
+
+    # Reverse image processing step
     img = img.permute(1, 2, 0).contiguous().numpy()
     img += [123, 117, 104]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    # Scale boxes back to image size
     boxes = boxes.numpy() * 300
     labels = labels.numpy()
 
